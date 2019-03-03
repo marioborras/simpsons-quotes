38e47e719dc3f97baaf867e457934675c43c1515
Mario Borras <41755574+marioborras@users.noreply.github.com>
2019-03-02 20:13:45 -0500

    Update main.js
    A small bit of  refactoring

--- main.js
+++ main.js
@@ -1,53 +1,45 @@
-const quoteEl = document.querySelector("#quote")
-const character = document.querySelector("#character")
-const charImg = document.querySelector("#char-image")
-const button = document.querySelector("#button")
+const quoteEl = document.querySelector('#quote');
+characterEl = document.querySelector('#character');
+charImg = document.querySelector('#char-image');
+button = document.querySelector('#button');
 
 const randomize = () => {
-    let rgbcolor
-    red = Math.floor(Math.random() * 250 + 0)
-    green = Math.floor(Math.random() * 250 + 0)
-    blue = Math.floor(Math.random() * 250 + 0)
-  
-    rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')'
-    document.body.style.background = rgbColor
-    button.style.background = rgbColor
-    quoteEl.style.color = rgbcolor
+  let rgbcolor;
+  red = Math.floor(Math.random() * 250 + 0);
+  green = Math.floor(Math.random() * 250 + 0);
+  blue = Math.floor(Math.random() * 250 + 0);
 
+  rgbColor = `rgb(${red},${green},${blue})`;
+  document.body.style.background = rgbColor;
+  button.style.background = rgbColor;
+  quoteEl.style.color = rgbcolor;
 
-  
-    red = ("0" + red.toString(16)).substr(-2).toUpperCase()
-    green = ("0" + green.toString(16)).substr(-2).toUpperCase()
-    blue = ("0" + blue.toString(16)).substr(-2).toUpperCase()
+  red = (`0` + red.toString(16)).substr(-2).toUpperCase();
+  green = (`0` + green.toString(16)).substr(-2).toUpperCase();
+  blue = (`0` + blue.toString(16)).substr(-2).toUpperCase();
+};
+
+const getQuote = async () => {
+  const response = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+  if (response.status === 200) {
+    return response.json();
+  } else {
+    throw new Error('unable to fetch the quote');
   }
-  
- 
-const getQuote = async()=> {
-    const response = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes")
-    if (response.status === 200) {
-        return response.json()
-    }else {
-        throw new Error("unable to fetch the quote")
-    }
-}
+};
 
+const loadQuote = async () => {
+  let [quoteData] = await getQuote();
+  const { quote, character, image } = quoteData;
+  quoteEl.textContent = `"${quote}"`;
+  characterEl.textContent = `- ${character}`;
+  charImg.src = image;
+  twitter.href = `https://twitter.com/intent/tweet?text=${quote}" - ${character}`;
+  randomize();
+};
 
-const startQuote = async () => {
-    let quote = await getQuote()
-    quote = quote[0]
-    quoteEl.textContent = `"${quote.quote}"`
-    character.textContent = `- ${quote.character}`
-    charImg.src = quote.image
-    twitter.href =`https://twitter.com/intent/tweet?text=${quote.quote}" - ${quote.character}`
-    randomize()
-    
-}
+loadQuote();
 
-startQuote()
-
-document.querySelector("#button").addEventListener("click",()=> {
-    console.log("clicked")
-    startQuote()
-
-})
-
+document.querySelector('#button').addEventListener('click', () => {
+  loadQuote();
+});
